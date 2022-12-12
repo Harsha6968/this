248949aae27c160e2622e315ce372903b68f6bac
harsha6968 <goneharshavardhan96962@gmail>
2022-12-12 13:11:38 +0530

    okie

---
+++ two.js
@@ -0,0 +1,15 @@
+class Employee{
+    /*emp_id=""
+    emp_name=""
+    emp_sal=""*/
+    constructor(id,name,sal){
+        this.emp_id=id;
+        this.emp_name=name;
+        this.emp_sal=sal;
+
+    }
+}
+let a1=new Employee(101,"harsha",45000)
+let a2=new Employee(102,"raaji",55000)
+console.log(a1)
+console.log(a2)
